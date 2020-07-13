67a5f68bf4aa02fb9b3d422546cce9ac4d630178
Spencer <spencermehta@gmail.com>
2020-07-13 19:54:48 +0100

    fixed merge

--- projects/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', ProjectList.as_view(), name="project-list"),
-    path('project-detail/<int:pk>/', ProjectDetail.as_view(), name="project-detail"),
-]
